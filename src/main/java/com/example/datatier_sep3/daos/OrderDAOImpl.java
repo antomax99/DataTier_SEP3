30418f428b4e60439bddb9bb399977d2c37fe7cb
antom <antomax.9960@gmail.com>
2022-05-31 19:19:48 +0200

    -getOrderbyId

--- src/main/java/com/example/datatier_sep3/daos/OrderDAOImpl.java
+++ src/main/java/com/example/datatier_sep3/daos/OrderDAOImpl.java
@@ -7,6 +7,7 @@ import com.example.datatier_sep3.entities.Product;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 //TODO: Test all methods
 public class OrderDAOImpl implements OrderDAO{
@@ -54,83 +55,78 @@ public class OrderDAOImpl implements OrderDAO{
         return OrdersFound;
     }
 
-    //TODO: Add products to order
     @Override
     public List<Order> getOrdersFromUser(int userID) throws IOException {
 
         List<Order> OrdersFound = new ArrayList<>();
         try(Connection connection = getConnection()) {
-
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM public.order o INNER JOIN public.users u on o.\"customerId\"=u.id where u.id="+userID);
             ResultSet resultSet = statement.executeQuery();
-
             while (resultSet.next()){
-                int id = resultSet.getInt("id");
-                System.out.println(id);
+                int orderId = resultSet.getInt("id");
                 int customerId = resultSet.getInt("customerId");
-                Double price = resultSet.getDouble("price");
-                boolean isCompleted = resultSet.getBoolean("completed");
-                Product p = new Product("name","test product brand","test product description", 245);
-                ArrayList<Product>products = new ArrayList<Product>();
-                products.add(p);products.add(p);products.add(p);products.add(p);
-                Order order = new Order(id,userID,price,products,isCompleted);
+                double price = resultSet.getDouble("price");
+                boolean completed = resultSet.getBoolean("completed");
+                Order order = new Order(orderId,customerId,price,null,completed);
                 OrdersFound.add(order);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        System.out.println(OrdersFound.toString());
         return OrdersFound;
 
     }
 
     @Override
     public Order getOrderById(int id) {
-
         Order orderFound = null;
         try(Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM database_sep3.public.order WHERE id = ?");
-            statement.setInt(1, id);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM database_sep3.public.order, database_sep3.public.order_products, database_sep3.public.products WHERE database_sep3.public.products.id = database_sep3.public.order_products.product_id AND database_sep3.public.order.id = database_sep3.public.order_products.order_id ");
             ResultSet resultSet = statement.executeQuery();
             if(resultSet.isBeforeFirst()){
                 if (resultSet.next()) {
-                    int customerId = resultSet.getInt("customerId");
-                    Double price = resultSet.getDouble("price");
-                    boolean isCompleted = resultSet.getBoolean("completed");
-                    //TODO: Get all attached products
-                    Product p = new Product("name","test product brand","test product description", 245);
-                    ArrayList<Product>products = new ArrayList<Product>();
-                    products.add(p);products.add(p);products.add(p);products.add(p);
-                    orderFound = new Order(id,customerId,price,products,isCompleted);
+                   int productId = resultSet.getInt("id");
+                   String name = resultSet.getString("name");
+                   String brand = resultSet.getString("brand");
+                   String description = resultSet.getString("description");
+                   double value = resultSet.getDouble("value");
+                   Product productFound = new Product(productId,name,brand,description,value);
+                   List<Product> products = new ArrayList<>();
+                   products.add(productFound);
+
+                   int orderId = resultSet.getInt("id");
+                   int customerId = resultSet.getInt("customerId");
+                   double price = resultSet.getDouble("price");
+                   boolean completed = resultSet.getBoolean("completed");
+                   orderFound = new Order(orderId,customerId,price,products,completed);
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return orderFound;
     }
 
     @Override
     public void addOrder(Order order) {
-        Order result = null;
+        List<Product> products = order.getProducts();
         try (Connection connection = getConnection()) {
+
+            for(int i = 0; i < products.size(); i ++)
+            {
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO database_sep3.public.order_products (order_id,product_id) VALUES (?,?)");
+                statement.setInt(1,order.getOrderId());
+                statement.setInt(2,products.get(i).getProductId());
+            }
+
             //TODO: Add products to order
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO database_sep3.public.order (orderId,customerId,price, isCompleted, xxx, ) VALUES (?,?, ?, ?, ?)",  PreparedStatement.RETURN_GENERATED_KEYS);
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO database_sep3.public.order (orderId,customerId,price, isCompleted) VALUES (?,?,?,?)",  PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, order.getOrderId());
             statement.setInt(2, order.getCustomerId());
             statement.setDouble(3, order.getPrice());
             statement.setBoolean(4, order.isCompleted());
-            //statement.setString(5, order.getProducts());
             statement.executeUpdate();
 
-            ResultSet keys = statement.getGeneratedKeys();
-            if (keys.next()) {
-                result = new Order (keys.getInt(1), order.getCustomerId() , order.getPrice(), order.isCompleted());
-            } else {
-                throw new SQLException("No key generated");
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
